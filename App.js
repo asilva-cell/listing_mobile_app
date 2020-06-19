48aade1f6a7cb12a901fb9acc0fbe126f0650291
asilva-cell <achapellin@gmail.com>
2020-06-19 17:46:38 -0600

    customized appTextInput component

--- App.js
+++ App.js
@@ -6,7 +6,13 @@ import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import Screen from "./app/components/Screen";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  return <ListingsScreen />;
+  return (
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email" />
+    </Screen>
+  );
 }
--- app/config/colors.js
+++ app/config/colors.js
@@ -5,6 +5,6 @@ export default {
   white: "#fff",
   medium: "#6e6969",
   light: "#e6e4e5",
+  dark: "#0c0c0c",
   danger: "#ff5252",
-  
 };
